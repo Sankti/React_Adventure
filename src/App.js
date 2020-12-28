5be5df3f1618b6dd80f8d3cf23fd44224291a78e
Adam <57396704+Sankti@users.noreply.github.com>
2020-12-28 16:58:59 +0100

    Modifying App to a class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      playerClass: null,
+      playerHpMax: 5,
+      playerHp: 5,
+      playerManaMax: 5,
+      playerMana: 5
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello.</h1>
+      </div> 
+    );
+  };
 }
 
 export default App;
