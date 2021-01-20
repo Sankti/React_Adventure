a1cc584c21e5356609e6a1ce957bbe63c43c3065
Adam <57396704+Sankti@users.noreply.github.com>
2021-01-20 21:47:29 +0100

    Implementing a switch conditional function

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends React.Component {
     super();
 
     this.state = {
-      chosenDistricts: [],
       bialoleka: false,
       bielany: false,
       bemowo: false,
@@ -34,36 +33,53 @@ class App extends React.Component {
     };
   };
 
-  addDistrict = (district) => {
-    this.setState(
-      {chosenDistricts: [...this.state.chosenDistricts, district]}
-    );
-  }
+  // addDistrict = (district) => {
+  //   this.setState(
+  //     {chosenDistricts: [...this.state.chosenDistricts, district]}
+  //   );
+  // }
 
-  removeDistrict = (district) => {
-    const index = this.state.chosenDistricts.indexOf(district)
+  // removeDistrict = (district) => {
+  //   let index = this.state.chosenDistricts.indexOf(district)
 
-    if (index > -1) {
-      this.setState(
-        {chosenDistricts: [this.state.chosenDistricts.splice(
-          index, 1
-          )]}
-      )}
-  }
+  //   if (index > -1) {
+  //     this.setState(
+  //       {chosenDistricts: [this.state.chosenDistricts.splice(
+  //         index, 1
+  //         )]}
+  //     )}
+  // };
+
+  // districtClick = (event) => {
+  //   const clickedDistrict = event.target.id;
+  //   console.log(clickedDistrict);
+  //   console.log(typeof(clickedDistrict))
+
+  //   if (this.state.chosenDistricts.includes(clickedDistrict)) {
+  //     this.removeDistrict(clickedDistrict);
+  //   } else {
+  //     this.addDistrict(clickedDistrict);
+  //   };
+  //   // Delete this upon resolving the debug:
+  //   console.log(this.state);
+  // };
 
   districtClick = (event) => {
-    const clickedDistrict = event.target.id;
-    console.log(clickedDistrict);
-    console.log(typeof(clickedDistrict))
+    let district = event.target.id;
+    console.log(district);
 
-    if (this.state.chosenDistricts.includes(clickedDistrict)) {
-      this.removeDistrict(clickedDistrict);
-    } else {
-      this.addDistrict(clickedDistrict);
+    switch(district) {
+      case "bialoleka":
+        this.setState({bialoleka: !this.state.bialoleka});
+        break;
+      case "bielany":
+        this.setState({bielany: !this.state.bielany});
+        break;
+      default:
+        console.log("No district selected.")
     };
-    // Delete this upon resolving the debug:
     console.log(this.state);
-  };
+  }
 
   executeClick = () => {
     window.open("http://sankti.github.io/");
@@ -77,7 +93,7 @@ class App extends React.Component {
           <div className="user-panel">
             <p>
               Kliknij na dzielnice, dla których chcesz zobaczyć ogłoszenia.<br />
-              Wybrane dzielnice: {this.state.chosenDistricts.join(", ")}
+              {/* Wybrane dzielnice: {this.state.chosenDistricts.join(", ")} */}
             </p>
             <UserForm />
             <ButtonExecute pushFunction={this.executeClick.bind(this)} />
