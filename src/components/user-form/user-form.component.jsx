cfd84ed38322dec3a723b1123fc1c9ed1f79abee
Adam <57396704+Sankti@users.noreply.github.com>
2021-01-22 16:50:32 +0100

    Update user-form.component.jsx

--- src/components/user-form/user-form.component.jsx
+++ src/components/user-form/user-form.component.jsx
@@ -4,7 +4,7 @@ const UserForm = () => (
     <form className='user-form'>
         Dodatkowe tagi: <input type="text"></input><br />
         Wybierz, jakie wyszukiwarki uwzględnić w zapytaniu:<br />
-        <input type="checkbox" /> Gumtree<br />
+        <input type="checkbox" onClick={console.log("Test successful")} /> Gumtree<br />
         <input type="checkbox" /> OLX<br />
         <input type="checkbox" /> Otodom
     </form>
